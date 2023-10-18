0e441eb4604155c08263ed376a166441482939ff
Supawitsaelim <oatsupawit_159@outlook.com>
2023-10-18 14:56:05 +0700

    up

--- app.py
+++ app.py
@@ -1,17 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from pysnmp.hlapi.asyncio.slim import Slim
 from pysnmp.smi.rfc1902 import ObjectIdentity, ObjectType
 import asyncio
 
 app = Flask(__name__)
 
-async def get_snmp_data():
+async def get_snmp_data(target_ip, community_string):
     slim = Slim(1)
 
-    target_ip = '10.4.15.32'
-    community_string = 'public'
-
-    # Retrieve sysUpTime
     errorIndication1, errorStatus1, errorIndex1, varBinds1 = await slim.get(
         community_string,
         target_ip,
@@ -19,7 +15,6 @@ async def get_snmp_data():
         ObjectType(ObjectIdentity("SNMPv2-MIB", "sysUpTime", 0)),
     )
 
-    # Retrieve sysDescr
     errorIndication2, errorStatus2, errorIndex2, varBinds2 = await slim.get(
         community_string,
         target_ip,
@@ -50,8 +45,17 @@ async def get_snmp_data():
 
 @app.route('/')
 def index():
-    snmp_data = asyncio.run(get_snmp_data())
-    return render_template('index.html', snmp_data=snmp_data)
+    return render_template('index.html', snmp_data=None)
+
+@app.route('/ip_address', methods=['GET', 'POST'])
+def get_ip_address():
+    if request.method == 'POST':
+        print(request.form['ip_address'])
+        target_ip = request.form['ip_address']
+        community_string = 'public'  # You can change this to the appropriate community string
+        snmp_data = asyncio.run(get_snmp_data(target_ip, community_string))
+        return render_template('index.html', snmp_data=snmp_data)
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
